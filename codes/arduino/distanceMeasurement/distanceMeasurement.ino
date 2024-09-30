b2dd1d3aead64b8a83d028d21bcc4a6b1416f4e5
Kevin <kevin45christian@gmail.com>
2024-09-30 11:35:12 +1000

    Update Arduino to works with 6 Ultrasonic Sensors

--- codes/arduino/distanceMeasurement/distanceMeasurement.ino
+++ codes/arduino/distanceMeasurement/distanceMeasurement.ino
@@ -1,18 +1,43 @@
-#define TRIG_PIN_1 4
-#define ECHO_PIN_1 3
-#define TRIG_PIN_2 13
-#define ECHO_PIN_2 12
+#define P1_ECHO_PIN_1 2
+#define P1_TRIG_PIN_1 3
+#define P1_ECHO_PIN_2 4
+#define P1_TRIG_PIN_2 5
+#define P1_ECHO_PIN_3 6
+#define P1_TRIG_PIN_3 7
+
+#define P2_ECHO_PIN_1 8
+#define P2_TRIG_PIN_1 9
+#define P2_ECHO_PIN_2 10
+#define P2_TRIG_PIN_2 11
+#define P2_ECHO_PIN_3 12
+#define P2_TRIG_PIN_3 13
 
 void setup() {
   Serial.begin(9600);
   
-  // Sensor 1 pins
-  pinMode(TRIG_PIN_1, OUTPUT);
-  pinMode(ECHO_PIN_1, INPUT);
+  // Player 1, Sensor 1
+  pinMode(P1_ECHO_PIN_1, INPUT);
+  pinMode(P1_TRIG_PIN_1, OUTPUT);
 
-  // Sensor 2 pins
-  pinMode(TRIG_PIN_2, OUTPUT);
-  pinMode(ECHO_PIN_2, INPUT);
+  // Player 1, Sensor 2
+  pinMode(P1_ECHO_PIN_2, INPUT);
+  pinMode(P1_TRIG_PIN_2, OUTPUT);
+
+  // Player 1, Sensor 3
+  pinMode(P1_ECHO_PIN_3, INPUT);
+  pinMode(P1_TRIG_PIN_3, OUTPUT);
+
+  // Player 2, Sensor 1
+  pinMode(P2_ECHO_PIN_1, INPUT);
+  pinMode(P2_TRIG_PIN_1, OUTPUT);
+
+  // Player 2, Sensor 2
+  pinMode(P2_ECHO_PIN_2, INPUT);
+  pinMode(P2_TRIG_PIN_2, OUTPUT);
+
+  // Player 2, Sensor 3
+  pinMode(P2_ECHO_PIN_3, INPUT);
+  pinMode(P2_TRIG_PIN_3, OUTPUT);
 }
 
 long getDistance(int trigPin, int echoPin) {
@@ -29,13 +54,27 @@ long getDistance(int trigPin, int echoPin) {
 }
 
 void loop() {
-  long distance1 = getDistance(TRIG_PIN_1, ECHO_PIN_1);
-  long distance2 = getDistance(TRIG_PIN_2, ECHO_PIN_2);
+
+  long p1d1 = getDistance(P1_TRIG_PIN_1, P1_ECHO_PIN_1);
+  long p1d2 = getDistance(P1_TRIG_PIN_2, P1_ECHO_PIN_2);
+  long p1d3 = getDistance(P1_TRIG_PIN_3, P1_ECHO_PIN_3);
+
+  long p2d1 = getDistance(P2_TRIG_PIN_1, P2_ECHO_PIN_1);
+  long p2d2 = getDistance(P2_TRIG_PIN_2, P2_ECHO_PIN_2);
+  long p2d3 = getDistance(P2_TRIG_PIN_3, P2_ECHO_PIN_3);
   
-  // Send the distances in the format: distance1,distance2
-  Serial.print(distance1);
+  // Send the distances in the format: p1d1,p1d2,p1d3,p2d1,p2d2,p2d3
+  Serial.print(p1d1);
   Serial.print(",");
-  Serial.println(distance2);
+  Serial.print(p1d2);
+  Serial.print(",");
+  Serial.print(p1d3);
+  Serial.print(",");
+  Serial.print(p2d1);
+  Serial.print(",");
+  Serial.print(p2d2);
+  Serial.print(",");
+  Serial.println(p2d3);
 
   delay(1000);
 }
